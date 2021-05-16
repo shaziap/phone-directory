cb32442a12dc985a0982ebfe70c5e51344d0ccd6
shaziap <shaiz.sit@gmail.com>
2021-05-16 15:45:00 +0530

    Styled Header External Styling

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./Header.css";
 const Header = function () {
 const headerStyle = {
     textAlign: 'center',
@@ -9,7 +10,7 @@ const headerStyle = {
  };
  return (
     
-    <div style={headerStyle}>
+    <div className="header">
         Phone Directory
     </div>
 )
