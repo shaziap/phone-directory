a9260a14cd78ba84322ca3e2db2c15349f8a1291
shaziap <shaiz.sit@gmail.com>
2021-05-16 15:50:17 +0530

    Styled Header External Styling

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import "./Header.css";
 const Header = function () {
-const headerStyle = {
-    textAlign: 'center',
-    padding: 20,
-    background: '#000',
-    color: '#fff',
-    textTransform: 'uppercase'
- };
  return (
     
     <div className="header">
