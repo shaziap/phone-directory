78501e91e0d08ef943690c734b8c65ca64ccd5e2
shaziap <shaiz.sit@gmail.com>
2021-05-18 13:54:32 +0530

    “Added routing for Add Subscriber page and Show Subscribers page”

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import './AddSubscriber.css';
-
+import { Link } from 'react-router-dom';
 class AddSusbscriber extends Component {
     constructor() {
         super();
@@ -21,6 +21,7 @@ class AddSusbscriber extends Component {
         e.preventDefault();
         this.props.addSubscriberHandler(this.state);
         this.setState({ id: 0, name: '', phone: ' ' });
+        this.props.history.push("/");
     }
 
     render() {
@@ -29,7 +30,7 @@ class AddSusbscriber extends Component {
             <div>
                 <Header heading="Add Subscriber" />
                 <div className="component-body-container">
-                    <button className="custom-btn">Back</button>
+                <Link to="/">   <button className="custom-btn">Back</button></Link>
 
                     <form className="subscriber-form" onSubmit={this.onFormSubmitted.bind(this)}>
                         <label htmlFor="name" className="label-control">Name: </label><br/>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import PhoneDirectory from './PhoneDirectory';
-import AddSusbscriber from './AddSubscriber';
 import './common/common.css';
 
 ReactDOM.render(
